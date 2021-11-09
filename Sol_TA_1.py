42858a67cbcfbedf4e6020b10721b2d306e0ae5d
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-11-09 20:13:30 +0530

    Create Sol_TA_1.py

---
+++ Sol_TA_1.py
@@ -0,0 +1,17 @@
+str1="Hello"
+list1=[1,2,3,"Sam",5,6,7,"Tom"]
+dict1={"Name":"Sam", "Roll_No":1}
+
+n1=10
+n2=10.05
+
+print("\n",type(str1))
+print("\n",type(list1))
+print("\n",type(dict1))
+print("\n",type(n1))
+print("\n",type(n2))
+print("\n",type(n2/n1))
+
+n3=int(n2/n1)
+
+print("\n",type(n3))
